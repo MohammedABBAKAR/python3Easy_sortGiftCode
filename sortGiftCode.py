b08f5d03f42bd47415418021b1e9765d4abc665e
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2024-07-16 07:13:12 +0200

    sortGiftCode2

--- sortGiftCode.py
+++ sortGiftCode.py
@@ -23,3 +23,20 @@ print(sortGiftCode("cabo"))
 #     return x
 
 # print(sortGiftCode("cabo"))
+
+# sorting algorithm manually. Here's a version of sortGiftCode that 
+# uses the Bubble Sort algorithm
+def sortGiftCode(n):
+    # Convert the string to a list of characters
+    chars = list(n)
+    # Bubble sort algorithm
+    for i in range(len(chars)):
+        for j in range(0, len(chars) - i - 1):
+            if chars[j] > chars[j + 1]:
+                # Swap the characters
+                chars[j], chars[j + 1] = chars[j + 1], chars[j]
+    # Convert the list back to a string and return
+    return ''.join(chars)
+
+# Test cases
+print(sortGiftCode("abcdef"))
